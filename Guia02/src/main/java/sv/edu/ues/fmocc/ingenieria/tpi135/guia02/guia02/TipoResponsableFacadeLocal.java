f7c5622936fdf12e1cfe844f9916ee71c98872fd
IrvinAyala <hypleyone@gmail.com>
2018-04-06 01:47:46 -0600

    Add files via upload

---
+++ Guia02/src/main/java/sv/edu/ues/fmocc/ingenieria/tpi135/guia02/guia02/TipoResponsableFacadeLocal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.ues.fmocc.ingenieria.tpi135.guia02.guia02;
+
+import javax.ejb.Local;
+import sv.edu.ues.fmocc.ingenieria.tpi135.mantenimiento.mantenimientolib.entity.TipoResponsable;
+
+/**
+ *
+ * @author irvin
+ */
+@Local
+public interface TipoResponsableFacadeLocal extends AbstractFacadeInterface<TipoResponsable> {
+
+}
